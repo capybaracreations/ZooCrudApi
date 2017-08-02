72ec05e63d27484d1e4024e3e6ee1000c08851f4
Patryk Krawczyk <capybaracreations@gmail.com>
2017-08-02 19:51:09 +0200

    initial minimal spring application

---
+++ src/test/java/pro/patrykkrawczyk/zoocrudapi/ApplicationTest.java
@@ -0,0 +1,17 @@
+package pro.patrykkrawczyk.zoocrudapi;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class ApplicationTest {
+
+    @Test
+    public void exampleTest() {
+        Assert.assertEquals(4, 2 + 2);
+    }
+}
